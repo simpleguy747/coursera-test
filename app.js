4eec74212e508cc1d3ba35e9222cf3272a077252
simpleguy747 <simpleguy747@gmail.com>
2017-02-13 02:08:41 +0530

    Changed Name

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
     'use strict'
     angular.module('myFirstApp',[])
     .controller('MyFirstController',function($scope){
-        $scope.name="Gurudas";
+        $scope.name="SimpleGuy";
     });
 })()
